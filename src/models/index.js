e0da1b044f3b7d64a83bca1660227fb6cf3bd813
Rajiv Basnet <rajivbasnet1@gmail.com>
2025-11-21 14:12:15 -0800

    Remove duplicate src directory

--- src/models/index.js
+++
@@ -1,7 +0,0 @@
-// Models index file
-// This will export all data models
-
-// Models will be added in future phases
-// export { default as Professor } from './professor.model.js';
-// export { default as EmailTemplate } from './emailTemplate.model.js';
-
